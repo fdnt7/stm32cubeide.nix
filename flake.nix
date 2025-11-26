9f7827d21797246dce0e47e9ce8e771661373dca
qdlmcfresh <qdlmcfresh@gmail.com>
2025-11-26 12:59:08 +0100

    Refactor flake.nix outputs, add devshell

--- flake.nix
+++ flake.nix
@@ -5,15 +5,27 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      stm32cubeide = pkgs.callPackage ./stm32cubeide.nix { };
+    in
+    {
+      packages.${system}.default = stm32cubeide;
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${stm32cubeide}/bin/stm32cubeide";
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        name = "stm32cubeide-shell";
+        packages = [ stm32cubeide ];
+      };
     };
-    stm32cubeide = pkgs.callPackage ./stm32cubeide.nix {};
-  in
-  {
-    packages.${system}.default = stm32cubeide;
-  };
 }
